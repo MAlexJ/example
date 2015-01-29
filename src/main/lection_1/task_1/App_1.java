bf040faf72b54cbabf08a0e06105520ca0b95582
Alex <alexmaximovj@gmail.com>
2015-01-29 22:55:47 +0200

    new task Обмен значениями без дополнительной переменной

---
+++ src/main/lection_1/task_1/App_1.java
@@ -0,0 +1,23 @@
+package task_1;
+
+import org.apache.log4j.Logger;
+
+/**
+ * Created by admin on 29.01.2015.
+ */
+public class App_1{
+    private final static Logger LOG = Logger.getLogger(App.class);
+
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 5;
+
+        a = a + b; // a = 8, b = 5
+        b = a - b; // a = 8, b = 3
+        a = a - b; // a = 5, b = 3
+
+        LOG.debug("a = " + a);
+        LOG.debug("b = " + b);
+
+    }
+}
