70fe234b5b9ecbd82b3a8cdd7238d5b3d9d0b883
Alex <alexmaximovj@gmail.com>
2015-01-29 23:56:25 +0200

    Обмен значениями без дополнительной переменной Для int/long существует множество решений (??? byte, char, short) и даже такое потрясающее как (???)

---
+++ src/main/lection_1/task_1/App_2.java
@@ -0,0 +1,22 @@
+package task_1;
+
+import org.apache.log4j.Logger;
+
+public class App_2 {
+    private final static Logger LOG = Logger.getLogger(App.class);
+
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 5;
+
+        a = a ^ b;
+        LOG.debug("a - " + a);
+        b = a ^ b;
+        LOG.debug("b - " + b);
+        a = a ^ b;
+        LOG.debug("a - " + a);
+        LOG.debug("a = " + a);
+
+        LOG.debug("b = " + b);
+    }
+}
