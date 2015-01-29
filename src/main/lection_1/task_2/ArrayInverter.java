ec6ecd2f986ce0d372a24405391e1cc3b41e6116
Alex <alexmaximovj@gmail.com>
2015-01-30 00:04:47 +0200

    Алгоритм: инвертирование массива Одна из популярных задачек на собеседовании – написать метод, инвертирующий элементы массива: Я использую обмен значениями через дополнительную переменную
    public class ArrayInverter {
        public static void invert(int[] arr) {
            for (int k = 0; k < arr.length / 2; k++) {
                int tmp = arr[k];
                arr[k] = arr[arr.length - k - 1];
                arr[arr.length - k - 1] = tmp;
            }
        }
    }

---
+++ src/main/lection_1/task_2/ArrayInverter.java
@@ -0,0 +1,20 @@
+package task_2;
+
+import org.apache.log4j.Logger;
+
+
+/**
+ * Created by admin on 29.01.2015.
+ */
+public class ArrayInverter {
+    private final static Logger LOG = Logger.getLogger(ArrayInverter.class);
+
+    public static void main(String[] args) {
+        LOG.debug("main");
+    }
+
+    public static void invert(int[] arr) {
+
+    }
+
+}
