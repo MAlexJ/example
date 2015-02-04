643e796aee2203b08834a72f07870ff91eb54daf
Alex <alexmaximovj@gmail.com>
2015-02-04 10:57:18 +0300

    List<String> list = new ArrayList<>();

---
+++ src/main/lection_13_Collection/App_01_List.java
@@ -0,0 +1,49 @@
+import org.apache.log4j.Logger;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+/**
+ * Created by Alex on 04.02.2015.
+ */
+public class App_01_List {
+    private final static Logger LOG = Logger.getLogger(App_01_List.class);
+
+    public static void main(String[] args) {
+        LOG.debug("main");
+        List<String> list = new ArrayList<>();
+        LOG.debug("list.size() ->" + list.size());
+        LOG.debug("list.isEmpty() ->" + list.isEmpty());
+
+        // list.add("a");
+        list.add("a");
+        list.add("b");
+        list.add("c");
+        list.add("d");
+        list.add("a");
+        list.add("a");
+        list.add("a");
+        list.add("a");
+
+        // list.add(0, "first_1"); ----> insert
+        list.add(0, "first_1");
+        list.add(0, "first_2");
+
+        LOG.debug("list.contains(\"x\") ->" + list.contains("x"));
+        LOG.debug("list.contains(\"a\") ->" + list.contains("a"));
+        LOG.debug("list.get(3) -> " + list.get(3));
+
+        LOG.debug(list);
+
+        //remove for index
+        LOG.debug("list.remove(3) -> " + list.remove(3));
+
+        //remove for Object
+        LOG.debug("list.remove(\"a\") ->" + list.remove("a"));
+
+        LOG.debug(list);
+
+    }
+
+}
