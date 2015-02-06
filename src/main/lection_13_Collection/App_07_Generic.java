2f6e8ddaaeb5e0a0b5f99eb29881db49a964619d
Alex <alexmaximovj@gmail.com>
2015-02-06 14:20:31 +0300

    Ихние предки   ArrayList<? super Integer>

---
+++ src/main/lection_13_Collection/App_07_Generic.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+
+/**
+ * Created by Alex on 06.02.2015.
+ */
+public class App_07_Generic {
+
+    public static void main(String[] args) {
+
+        ArrayList<? extends Integer> ref0 = null;
+        ArrayList<? extends Number> ref1 = null;
+
+        // error ->  ref0=ref1;
+        ref1=ref0;
+
+
+        ArrayList<? super Integer> ref2 = null;
+        ArrayList<? super Number> ref3 = null;
+
+        ref2=ref3;
+        // error ->  ref3=ref2;
+
+    }
+
+}
