b397fd6ef2d834a1e2693e2e1ec70cac81012774
malexj <alexmaximovj@gmail.com>
2015-02-11 11:16:23 -0800

    task_1_test_00

---
+++ src/main/lecture_1/task_0/test/Test_00.java
@@ -0,0 +1,44 @@
+package task_0.test;
+
+/* Каким будет результат компиляции и запуска следующей программы?
+
+public class Test {
+    public static void main(String[] args) {
+        int i = 1;
+        long j = 1;
+        if (i == 1) {
+            int k = 1;
+            if (i == k) {
+                System.out.println("i equals k");
+            }
+        } else if (j == k) {
+            System.out.println("j equals k");
+        }
+    }
+}
+
+
+Answer:
+
+1.  Ошибка компиляции
+2.  i equals k
+3.  j equals k
+
+*/
+
+public class Test_00 {
+/*
+    public static void main(String[] args) {
+        int i = 1;
+        long j = 1;
+        if (i == 1) {
+            int k = 1;
+            if (i == k) {
+                System.out.println("i equals k");
+            }
+        } else if (j == k) {
+            System.out.println("j equals k");
+        }
+    }
+*/
+}
