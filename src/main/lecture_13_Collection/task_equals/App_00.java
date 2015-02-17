6bfe9775e94419fb77a1a086ef85251016d85a74
malexj <alexmaximovj@gmail.com>
2015-02-17 09:09:53 -0800

    fix lecture 13

--- src/main/lecture_13_Collection/task_equals/App_00.java
+++ src/main/lecture_13_Collection/task_equals/App_00.java
@@ -1,17 +1,20 @@
 package task_equals;
 
-import java.util.ArrayList;
-import java.util.List;
-
+/**
+ * Created by Alex on 2/17/2015.
+ */
 public class App_00 {
-
     public static void main(String[] args) {
-        List<Point2D> list = new ArrayList<>();
+        Integer i1 = Integer.valueOf(100);
+        Integer i2 = Integer.valueOf(100);
 
-        list.add(new Point2D(0, 0));
-        System.out.println(list.contains(new Point2D(0, 0)));
-        System.out.println(list.remove(new Point2D(0, 0)));
+        System.out.println(i1 == i2);
+        System.out.println(i1.equals(i2));
 
-        System.out.println(list);
+        //   Integer i3 = Integer.valueOf(200);
+        //  Integer i4= Integer.valueOf(200);
+        //   System.out.println(i3==i4);
+        //   System.out.println(i3.equals(i4));
+
     }
 }
--- src/main/lecture_13_Collection/task_equals/App_01.java
+++ src/main/lecture_13_Collection/task_equals/App_01.java
@@ -3,18 +3,15 @@ package task_equals;
 import java.util.ArrayList;
 import java.util.List;
 
-
 public class App_01 {
+
     public static void main(String[] args) {
+        List<Point2D> list = new ArrayList<>();
 
-        List<Point2D_Eql_Hash> list = new ArrayList<>();
-
-        list.add(new Point2D_Eql_Hash(0, 0));
-        System.out.println(list.contains(new Point2D_Eql_Hash(0, 0)));
-        System.out.println(list.remove(new Point2D_Eql_Hash(0, 0)));
+        list.add(new Point2D(0, 0));
+        System.out.println(list.contains(new Point2D(0, 0)));
+        System.out.println(list.remove(new Point2D(0, 0)));
 
         System.out.println(list);
-
-
     }
 }
