f64df2e74bfadafb53d386f5333a410513278b36
malexj <alexmaximovj@gmail.com>
2015-02-17 09:26:34 -0800

    fix lecture 13

--- src/main/lecture_13_Collection/task_Map/App_08_Map.java
+++ src/main/lecture_13_Collection/task_Map/App_08_Map.java
@@ -38,14 +38,26 @@ public class App_08_Map {
 
             //remove(K)
             /* return the previous value associated with key, or null if there was no mapping for key*/
-            LOG.debug(" map.remove(\"Килька\") -> " + map.remove(" илька"));
+            LOG.debug(" map.remove(\"Килька\") -> " + map.remove("Килька"));
 
             //remove(K, V);
-            LOG.debug(" map.remove(\"Сардина\", 10) -> " + map.remove("—ардина", 10));
+            LOG.debug(" map.remove(\"Сардина\", 10) -> " + map.remove("Сардина", 10));
 
             //size()
             LOG.debug("map.size() -> " + map.size());
 
+            //map.put(null,12)
+            LOG.debug("map.put(null,12) -> " + map.put(null,12));
+            LOG.debug("map.put(null,12) -> " + map.put(null,12));
+
+            //size()
+            LOG.debug("map.size() -> " + map.size());
+
+            //map.put("null",12)
+            LOG.debug("map.put(null,12) -> " + map.put("null",null));
+
+            //size()
+            LOG.debug("map.size() -> " + map.size());
 
         }
 
