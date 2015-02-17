51d1b0d123cb74a82f73c99e668cfa0a3aa232b7
malexj <alexmaximovj@gmail.com>
2015-02-17 07:20:04 -0800

    add -> lecture.task_String.App_03

---
+++ src/main/lecture_2/task_String/App_03.java
@@ -0,0 +1,23 @@
+package task_String;
+
+import java.util.Arrays;
+
+/**
+ * Created by Alex on 2/17/2015.
+ */
+public class App_03 {
+
+    public static void main(String[] args) {
+
+        //*****************************************
+        char[] arrChar = {'a', 'b', 'c', 'd'};
+        String str = new String(arrChar);
+        System.out.println(str);
+
+        //*****************************************
+        String str1 ="System.out.println(str);";
+        char[] chars = str1.toCharArray();
+        System.out.println(Arrays.toString(chars));
+
+    }
+}
