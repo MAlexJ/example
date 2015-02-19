01ace582758f56fd65cffd96bc61330ca20a79a0
Alex <alexmaximovj@gmail.com>
2015-02-19 10:40:11 +0300

    fix lecture_1 task_1

--- src/main/lecture_1/task_1/App.java
+++ src/main/lecture_1/task_1/App.java
@@ -4,7 +4,7 @@ import org.apache.log4j.Logger;
 
 /**
  * Created by admin on 29.01.2015.
- * <p/>
+ * <p>
  * Exchange values through an additional variable
  */
 public class App {
@@ -13,7 +13,10 @@ public class App {
     public static void main(String[] args) {
         int a = 3;
         int b = 5;
+        LOG.debug("a = " + a);
+        LOG.debug("b = " + b);
 
+        LOG.debug("********************");
         int tmp = a;
         a = b;
         b = tmp;
@@ -21,5 +24,42 @@ public class App {
         LOG.debug("a = " + a);
         LOG.debug("b = " + b);
 
+        LOG.debug("********************");
+
+        Dog mike = new Dog(2,"mike");
+        Dog hootDog = new Dog(3,"hootDog");
+
+        LOG.debug("mike = " + mike);
+        LOG.debug("hootDog = " + hootDog);
+        LOG.debug("********************");
+
+        //mike = hootDog;
+        //hootDog=mike;
+
+        Dog temp = mike;
+        mike = hootDog;
+        hootDog = temp;
+
+        LOG.debug("mike = " + mike);
+        LOG.debug("hootDog = " + hootDog);
+
+    }
+}
+
+class Dog {
+    private int age;
+    private String name;
+
+    public Dog(int age, String name) {
+        this.age = age;
+        this.name=name;
+    }
+
+    @Override
+    public String toString() {
+        return "Dog{" +
+                "age=" + age +
+                ", name='" + name + '\'' +
+                '}';
     }
 }
