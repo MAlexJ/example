1dc99b37c0af6b97a969e62039d617664fa33bfc
Alex <alexmaximovj@gmail.com>
2015-02-19 15:13:19 +0300

    fix

---
+++ src/main/lecture_1/task_0/App_prmitive_1.java
@@ -0,0 +1,23 @@
+package task_0;
+
+/**
+ * При сокращённых операциях в java производится приведение типов.
+ * То есть v1+=v2 аналогична v1=(int)(v1+v2)
+ * Соответственно в первой строке будет ошибка компиляции так как long шире чем int,
+ * а вторая строка успешно выполнится.
+ */
+
+public class App_prmitive_1 {
+
+    public static void main(String[] args) {
+        int numInt = 1;
+        long numLong = 2;
+
+        //??????
+        //numInt += numLong;
+        //?????
+        //numInt = numInt + numLong;
+
+    }
+}
+
