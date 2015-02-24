289bde148f09e41bbcfcee7d15aeaad6a3c348cc
Alex <alexmaximovj@gmail.com>
2015-02-24 11:52:52 +0300

    Add lecture_10_Polymorphism

---
+++ src/main/lecture_10_Polymorphism/task_1/App_04.java
@@ -0,0 +1,31 @@
+package task_1;
+
+/**
+ * Created by Alex on 24.02.2015.
+ */
+public class App_04 {
+
+    public static void main(String[] args) {
+
+        A4 a = new B4();
+        System.out.println("a.i -> "+a.i);
+        System.out.println("a.getI()-> "+ a.getI());
+    }
+}
+
+
+class A4 {
+    static int i = 3;
+
+   int getI() {
+        return i;
+    }
+}
+
+class B4 extends A4 {
+     int i = 5;
+
+     int getI() {
+        return i;
+    }
+}
