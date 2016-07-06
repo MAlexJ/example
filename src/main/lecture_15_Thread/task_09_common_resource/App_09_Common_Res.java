b8381c8ddc7a4b7846d124079e32c6bb8d9d10d2
malexj <alexmaximovj@gmail.com>
2016-07-06 22:55:16 +0300

    fix task Thread

---
+++ src/main/lecture_15_Thread/task_09_common_resource/App_09_Common_Res.java
@@ -0,0 +1,38 @@
+package task_09_common_resource;
+
+public class App_09_Common_Res {
+
+    public static void main(String[] args) {
+
+        CommonResource commonResource = new CommonResource();
+
+        for (int i=0;i<4;i++){
+            Thread thread = new Thread(new MyThread(commonResource),"Поток:");
+            thread.start();
+        }
+    }
+
+}
+
+//Common object
+class CommonResource {
+    int x = 0;
+}
+
+//My thread
+class MyThread implements Runnable {
+
+    private CommonResource common;
+
+    public MyThread(CommonResource common) {
+        this.common = common;
+    }
+
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 5; i++) {
+            System.out.println(Thread.currentThread().getName() + " : " + common.x++);
+        }
+    }
+}
---
+++ src/main/lecture_15_Thread/task_11_common_resource_synchronize_method/App_11_Common_Res_Synch_Method.java
@@ -0,0 +1,39 @@
+package task_11_common_resource_synchronize_method;
+
+public class App_11_Common_Res_Synch_Method {
+
+    public static void main(String[] args) {
+        CommonResource resource = new CommonResource();
+        for (int i = 0; i < 5; i++) {
+            Thread thread = new Thread(new MyThread(resource), "Thread #" + i);
+            thread.start();
+        }
+    }
+
+}
+
+//Common resource -> synchronize method
+class CommonResource {
+    private int x;
+
+    synchronized void setX() {
+        for (int i = 0; i < 5; i++) {
+            System.out.println(Thread.currentThread().getName() + " : " + this.x);
+            this.x++;
+        }
+    }
+}
+
+class MyThread implements Runnable {
+
+    private CommonResource resource;
+
+    MyThread(CommonResource resource) {
+        this.resource = resource;
+    }
+
+    @Override
+    public void run() {
+        this.resource.setX();
+    }
+}
