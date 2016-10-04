3bfc70a00a4c13c9ca5f0eaf4ea20be24be5d71b
malex <alexmaximovj@gmail.com>
2016-10-04 16:21:26 +0300

    Fix.  lambda expression

---
+++ src/main/lecture_15_Lambda/example_03/Main_Lambda_03.java
@@ -0,0 +1,26 @@
+package example_03;
+
+/**
+ * Simple example of using terminal lambda expression
+ *
+ * @author malex
+ */
+public class Main_Lambda_03 {
+   public static void main(String[] args) {
+      Lambdable lamb = s -> System.out.println(s);
+      lamb.print("Hello!");
+   }
+}
+
+/**
+ * Functional interface
+ */
+interface Lambdable {
+
+   /**
+    * Functional interface must contain only a single method with no implementation.
+    *
+    * @param str the incoming value.
+    */
+   void print(String str);
+}
