05e6b5958b1368febef90d8e103e3be1be5dbdb5
malex <alexmaximovj@gmail.com>
2016-10-06 18:59:51 +0300

    Fix.

--- src/main/lecture_15_Lambda/example_02/Main_Lambda_07.java
+++ src/main/lecture_15_Lambda/example_02/Main_Lambda_07.java
@@ -8,23 +8,30 @@ package example_02;
 public class Main_Lambda_07 {
 
    public static void main(String[] args) {
-      int[] nums = { -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5};
-      System.out.println(sum(nums, ExpressionHelper::isEven));
+      int[] nums = {-5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5};
 
-      Expression expr = ExpressionHelper::isPositive;
+      Expression_01 expr = ExpressionHelper::isEven;
+      System.out.println(sum(nums, expr));
+
+      expr = ExpressionHelper::isPositive;
       System.out.println(sum(nums, expr));
    }
 
-   private static int sum (int[] numbers, Expression func)
-   {
+   private static int sum(int[] numbers, Expression_01 func) {
       int result = 0;
-      for(int i : numbers)
-      {
+      for (int i : numbers) {
          if (func.isEqual(i))
             result += i;
       }
       return result;
    }
+}
+
+/**
+ * Functional interface.
+ */
+interface Expression_01 {
+   boolean isEqual(int n);
 }
 
 /**
