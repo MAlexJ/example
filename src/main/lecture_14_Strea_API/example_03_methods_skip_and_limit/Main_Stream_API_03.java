a99f55c0605fc2a4dcb09b31dfabe235d4484d15
malexj <alexmaximovj@gmail.com>
2016-10-22 19:16:48 +0300

    Added an example of using the method 'skip' and 'limit' of the Stream.

--- src/main/lecture_14_Strea_API/example_03_methods_skip_and_limit/Main_Stream_API_03.java
+++ src/main/lecture_14_Strea_API/example_03_methods_skip_and_limit/Main_Stream_API_03.java
@@ -1,5 +1,7 @@
 package example_03_methods_skip_and_limit;
 
+import java.util.stream.Stream;
+
 /**
  * @author malex
  */
@@ -7,7 +9,62 @@ public class Main_Stream_API_03 {
 
     public static void main(String[] args) {
 
+        // Skip
+        use_skip("Nokia 3110", "Samsung C100", "Alkatel e18", "HTC ONE");
+
+        // Limit
+        use_limit("1", "2", "3", "4", "5", "6");
     }
 
+
+    /**
+     * Skip stream
+     *
+     * @param args the array of phone name.
+     */
+    private static void use_skip(String... args) {
+        print("SKIP");
+
+        Stream<String> stringStream = Stream.of(args);
+        stringStream.skip(2).forEach(System.out::println);
+
+        print();
+    }
+
+    /**
+     * Limit stream
+     *
+     * @param args the array of name
+     */
+    private static void use_limit(String... args) {
+        print("LIMIT");
+
+        Stream<String> stringStream = Stream.of(args);
+        stringStream.limit(3).forEach(System.out::println);
+
+        print();
+    }
+
+    /**
+     * Print message in console
+     *
+     * @param args the message
+     */
+    private static void print(String... args) {
+        String str = "";
+        boolean end = false;
+
+        if (args.length > 0) {
+            str = args[0];
+        } else {
+            end = true;
+        }
+
+        System.out.println(">>>>>>>>>>>>> " + str + " <<<<<<<<<<<<<<< ");
+
+        if (end) {
+            System.out.println();
+        }
+    }
 
 }
