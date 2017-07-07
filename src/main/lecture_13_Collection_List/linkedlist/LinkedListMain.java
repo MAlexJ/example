196d0aa81027ee726e138ef421c53f671f6c1c43
malexj <alexmaximovj@gmail.com>
2017-07-08 00:49:57 +0300

    fix

--- src/main/lecture_13_Collection_List/linkedlist/LinkedListMain.java
+++ src/main/lecture_13_Collection_List/linkedlist/LinkedListMain.java
@@ -1,5 +1,7 @@
 package linkedlist;
 
+import java.util.LinkedList;
+
 /**
  * @author malex
  */
@@ -7,6 +9,25 @@ public class LinkedListMain {
 
 	public static void main(String[] args) {
 
+		// #1 create a new LinkedList
+		createLinkedList(1, 2, 3, 4, 5);
+
+	}
+
+	/**
+	 * Create a new LinkedList
+	 *
+	 * @param elements are simple numbers
+	 */
+	private static void createLinkedList(int... elements) {
+
+		LinkedList<Integer> result = new LinkedList<>();
+
+		for (int element : elements) {
+			result.add(element);
+		}
+
+		System.out.println(result);
 	}
 
 }
