b2329fefb2ba0106484039fc1bf8c34d2799bf5f
malexj <alexmaximovj@gmail.com>
2017-07-24 13:02:23 +0300

    fix task

--- src/main/lecture_13_Collection/task_Collections/App_09_Collections.java
+++ src/main/lecture_13_Collection/task_Collections/App_09_Collections.java
@@ -9,26 +9,55 @@ import java.util.List;
  */
 public class App_09_Collections {
 
+	/**
+	 * Run
+	 */
 	public static void main(String[] args) {
 
-		// #1 method: nCopies
-		methodNCopies();
+		// #1 method: nCopies(...)
+		methodNCopies(new Case("bla bla bal"), "Passed parameter is object.", 4);
+
+		// #2 method: nCopies(...)
+		// Passed parameter is null.
+		methodNCopies(null, "Passed parameter is null.", 10);
+
+		// #3 method: fill(...)
+		methodFill(new Case("bla bla bal"), "NEW NAME", "method fill:", 3);
+
 	}
 
 	/**
-	 * The methodCollections.nCopies
+	 * Use the method Collections.fill()
+	 *
+	 * @param ob      the entity's object
+	 * @param newName the new name entity
+	 * @param text    the text for displayed message
+	 * @param countOb the count a new Objects in the list
 	 */
-	private static void methodNCopies() {
+	private static void methodFill(Case ob, String newName, String text, int countOb) {
 
-		// #1 create Entity
-		Case cases = new Case("bla bla bal");
+		// #1 create a new list of the object
+		List<Case> caseList = methodNCopies(ob, text, countOb);
 
-		// #2 create a new list and fill it
-		List<Case> list = new ArrayList<>(Collections.nCopies(4, cases));
+		// #2 fill new objects to the list
+		Collections.fill(caseList, new Case(newName));
 
-		// #3 print the list
-		System.out.println(list);
+		// #3 Print list
+		System.out.println(text + "  " + caseList);
+	}
 
+	/**
+	 * The method Collections.nCopies
+	 */
+	private static List<Case> methodNCopies(Case cases, String text, int countObj) {
+
+		// #1 create a new list and fill it
+		List<Case> list = new ArrayList<>(Collections.nCopies(countObj, cases));
+
+		// #1 print the list
+		System.out.println(text + "  " + list);
+
+		return list;
 	}
 
 
