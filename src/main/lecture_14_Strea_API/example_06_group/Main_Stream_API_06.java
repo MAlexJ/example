919df9d559dcfccf612848e64a5fd4434e2fd756
malexj <alexmaximovj@gmail.com>
2017-11-02 23:29:17 +0200

    Added new example `map`

--- src/main/lecture_14_Strea_API/example_06_group/Main_Stream_API_06.java
+++ src/main/lecture_14_Strea_API/example_06_group/Main_Stream_API_06.java
@@ -12,58 +12,60 @@ import static util.StreamAPIUtil.print;
  */
 public class Main_Stream_API_06 {
 
-   public static void main(String[] args) {
-      //example_01
-      use_collect_grouping_by(new Phone("iPhone 6 S", "Apple", 600),
-              new Phone("Lumia 950", "Microsoft", 500),
-              new Phone("iPhone 5 S", "Apple", 450),
-              new Phone("Lumia 640", "Microsoft", 200));
+	public static void main(String[] args) {
+		//example_01
+		use_collect_grouping_by(
+				  new Phone("iPhone 6 S", "Apple", 600),
+				  new Phone("Lumia 950", "Microsoft", 500),
+				  new Phone("iPhone 5 S", "Apple", 450),
+				  new Phone("Lumia 640", "Microsoft", 200)
+		);
 
-   }
+	}
 
-   private static void use_collect_grouping_by(Phone... phones) {
-      print("Example_01");
+	private static void use_collect_grouping_by(Phone... phones) {
+		print("Example_01");
 
-      Stream<Phone> phoneStream = Stream.of(phones);
+		Stream<Phone> phoneStream = Stream.of(phones);
 
-      Map<String, List<Phone>> phonesByCompany = phoneStream.collect(
-              Collectors.groupingBy(Phone::getCompany));
+		Map<String, List<Phone>> phonesByCompany = phoneStream.collect(
+				  Collectors.groupingBy(Phone::getCompany));
 
-      for(Map.Entry<String, List<Phone>> item : phonesByCompany.entrySet()){
+		for (Map.Entry<String, List<Phone>> item : phonesByCompany.entrySet()) {
 
-         System.out.println(item.getKey());
-         for(Phone phone : item.getValue()){
+			System.out.println(item.getKey());
+			for (Phone phone : item.getValue()) {
 
-            System.out.println(phone.getName());
-         }
-         System.out.println("");
-      }
+				System.out.println(phone.getName());
+			}
+			System.out.println("");
+		}
 
-      print();
-   }
+		print();
+	}
 
-   private static class Phone {
+	private static class Phone {
 
-      private String name;
-      private String company;
-      private int price;
+		private String name;
+		private String company;
+		private int price;
 
-      Phone(String name, String comp, int price) {
-         this.name = name;
-         this.company = comp;
-         this.price = price;
-      }
+		Phone(String name, String comp, int price) {
+			this.name = name;
+			this.company = comp;
+			this.price = price;
+		}
 
-      String getName() {
-         return name;
-      }
+		String getName() {
+			return name;
+		}
 
-      int getPrice() {
-         return price;
-      }
+		int getPrice() {
+			return price;
+		}
 
-      String getCompany() {
-         return company;
-      }
-   }
+		String getCompany() {
+			return company;
+		}
+	}
 }
---
+++ src/main/lecture_25_Java_8/example_07_map/example_01/MapTest_01.java
@@ -0,0 +1,70 @@
+package example_07_map.example_01;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class MapTest_01 {
+
+	public static void main(String[] args) {
+
+		List<String> list = Stream.of("05/01/2017",
+				  "06/01/2017", "07/01/2017",
+				  "08/01/2017", "09/01/2017",
+				  "10/01/2017", "11/01/2017")
+				  .collect(Collectors.toList());
+
+		String collect = list
+				  .stream()
+				  .collect(Collectors.joining(","));
+		System.out.println("Split: " + collect);
+
+		List<Date> dates = list.stream()
+				  .map(DateFunction::formatStringToDate)
+				  .collect(Collectors.toList());
+		dates.forEach(System.out::println);
+
+		System.out.println("Weekend: ");
+
+		dates.stream()
+				  .filter(WeekendPredicate::isWeekend)
+				  .forEach(System.out::println);
+	}
+
+}
+
+interface DateFunction {
+
+	/**
+	 * Function -> String to date
+	 */
+	static Date formatStringToDate(String date) {
+		SimpleDateFormat format = new SimpleDateFormat("dd/mm/yyy");
+		try {
+			return format.parse(date);
+		} catch (ParseException e) {
+			System.out.println(e.getMessage());
+		}
+		return null;
+	}
+}
+
+interface WeekendPredicate {
+
+	/**
+	 * Check day -> SUNDAY or SATURDAY
+	 */
+	static boolean isWeekend(Date date) {
+		Calendar calendar = Calendar.getInstance();
+		calendar.setTime(date);
+
+		int nDay = calendar.get(Calendar.DAY_OF_WEEK);
+
+		return nDay == Calendar.SUNDAY
+				  || nDay == Calendar.SATURDAY;
+	}
+}
