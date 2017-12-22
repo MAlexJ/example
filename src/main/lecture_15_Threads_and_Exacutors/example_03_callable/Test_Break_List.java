255f180cd8f6d3caa91d6a015f5895e6bd3da614
malex <alexmaximovj@gmail.com>
2017-12-22 23:42:10 +0200

    fix example

--- src/main/lecture_15_Threads_and_Exacutors/example_03_callable/Test_Break_List.java
+++ src/main/lecture_15_Threads_and_Exacutors/example_03_callable/Test_Break_List.java
@@ -2,33 +2,45 @@ package example_03_callable;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
 public class Test_Break_List
 {
+    private static long startTime = System.currentTimeMillis();
     private static List<Integer> list = new ArrayList<>();
 
-    public static void main(String[] args)
+    public static void main(String[] args) throws InterruptedException
     {
-        for (int i = 0; i <= 53; i++)
+        for (int i = 0; i <= 5000000; i++)
         {
             list.add(i);
         }
-        newExecuteTask(list, 6);
+//        newExecuteTask(list, 100);
+        newExecuteTaskSingle(list);
     }
 
-    private static void newExecuteTask(List<Integer> list, int numberThread)
+    private static void newExecuteTaskSingle(List<Integer> list) throws InterruptedException
     {
-        ExecutorService executorService = Executors.newFixedThreadPool(numberThread);
+        new Thread(task(list)).start();
+    }
+
+    private static void newExecuteTask(List<Integer> list, int numberThread) throws InterruptedException
+    {
+        // #1: create -> ExecutorService
+        List<Runnable> runnableList = new ArrayList<>();
+
+        // #2: Filling -> callableList
         for (int start = 0; start < list.size(); start = start + numberThread)
         {
             System.out.println(start + " " + (start + numberThread));
             int end = start + numberThread >= list.size() ? list.size() : start + numberThread;
-            executorService.submit(task(list.subList(start, end)));
+            runnableList.add(task(list.subList(start, end)));
         }
-        executorService.shutdown();
+
+        // #3: create thread
+        for (Runnable run : runnableList)
+        {
+            new Thread(run).start();
+        }
     }
 
     private static Runnable task(List<Integer> list)
@@ -38,11 +50,14 @@ public class Test_Break_List
             list.forEach(item ->
             {
                 System.out.println(item + " " + Thread.currentThread().getName());
-                for (int i = 0; i <= new Random().nextInt(Integer.MAX_VALUE); i++)
+                for (int i = 1; i <= Integer.MAX_VALUE - 2; i++)
                 {
                     int x = i;
                 }
+
             });
+            System.out.println("   >>> End time: " + (System.currentTimeMillis() - startTime));
         };
+
     }
 }
