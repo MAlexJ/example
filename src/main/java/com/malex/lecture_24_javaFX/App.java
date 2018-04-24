034b665934af67be243d773a33c6339090374825
malex <alexmaximovj@gmail.com>
2018-04-25 00:56:42 +0300

    fix

--- src/main/java/com/malex/lecture_24_javaFX/App.java
+++ src/main/java/com/malex/lecture_24_javaFX/App.java
@@ -1,4 +1,4 @@
-package example_01;
+package com.malex.lecture_24_javaFX;
 
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyCodeCombination;
@@ -7,8 +7,7 @@ import javafx.scene.input.KeyCombination;
 import java.util.HashMap;
 import java.util.Map;
 
-public class Main_01 {
-
+public class App {
     private static Map<KeyCombination, String> map = new HashMap<>();
 
     private static String A = "a";
@@ -20,21 +19,13 @@ public class Main_01 {
         map.put(new KeyCodeCombination(KeyCode.A, KeyCombination.SHIFT_ANY), A);
         map.put(new KeyCodeCombination(KeyCode.B, KeyCombination.SHIFT_ANY), B);
         map.put(new KeyCodeCombination(KeyCode.C, KeyCombination.SHORTCUT_ANY), C);
-        map.put(new KeyCodeCombination(KeyCode.D, null), D);
-
     }
 
-
     public static void main(String[] args) {
-
-
         for (KeyCombination combination: map.keySet()){
-
             if(combination.getShortcut()!= null){
                 System.out.println(map.get(combination));
             }
-
         }
-
     }
 }
