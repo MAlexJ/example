7f28985ef20f7e9fb315b66cb9e0e085fc7fa32d
malexj <alexmaximovj@gmail.com>
2018-12-17 22:47:53 +0200

    Updated random examples

---
+++ src/main/java/com/malex/lecture_5_random/GenerateRandomArraysOrList.java
@@ -0,0 +1,75 @@
+package com.malex.lecture_5_random;
+
+import java.util.Arrays;
+import java.util.Random;
+import java.util.stream.IntStream;
+import lombok.extern.log4j.Log4j;
+
+/**
+ * <b>Generate array of random values in java 8 of using Stream API</b>
+ *
+ * <p>Source:
+ * http://codippa.com/how-to-generate-an-array-of-random-integers-using-stream-api-java-8/
+ *
+ * <p>Let’s tweak in :
+ *
+ * <p>#1: An error of type `java.lang.IllegalArgumentException`: bound must be greater than origin
+ * is thrown when the lower and upper limit of the method ints(streamSize, lowerBound, upperBound)
+ * are same.
+ *
+ * <p>#2: If stream size is not supplied to `ints()` method then a `java.lang.OutOfMemoryError` is
+ * raised.
+ *
+ * <p>#3: Similar to `ints()` method, there are methods such as longs() and doubles() `in
+ * java.util.Random` class which generate a stream for long and double values respectively.
+ *
+ * <p>#4: Elements of the generated array might repeat themselves.
+ *
+ * <p>#5: If you want to generate a list instead of an array, modify the code slightly as
+ * random.ints(10, 25,50).boxed().collect(Collectors.toList()).
+ *
+ * <p>#6: `boxed()` method of the stream instance returned by the `ints()` method converts the
+ * integer primitives into their wrapper `Integer` equivalent.
+ *
+ * <p>RESULT: Now you have another concept up your sleeve.
+ */
+@Log4j
+public class GenerateRandomArraysOrList {
+
+  /*
+   * Following are the methods added to java.util.Random class which return a stream of integers :
+   *
+   * #1: `ints()` – Returns an unlimited stream of integers. This stream if directly converted to an array would result in java.lang.OutOfMemoryError.
+   *
+   * #2: `ints(streamSize)` – Returns a stream of specified size. This stream when converted to an array will return an array of size equal to the given stream size.
+   *
+   * #3: ints(streamSize, lowerBound, upperBound) – Returns a stream of specified size.
+   * This stream when converted to an array will create an array of specified size with integers starting from lower bound value
+   * and less than upper bound value.
+   *
+   * #4: ints(lowerBound, upperBound) – Same as above except the returned stream is unlimited.
+   * This stream if directly converted to an array would result in java.lang.OutOfMemoryError.
+   */
+  public static void main(String[] args) {
+    // => new Random().ints( num )
+    exampleInts();
+
+    // new Random().ints( size, origin, bound )
+    exampleIntsToList();
+  }
+
+  private static void exampleInts() {
+    log.debug("Example #1");
+    IntStream ints = new Random().ints(5);
+    log.debug("new Random().ints(10); => " + Arrays.toString(ints.toArray()) + "\n");
+  }
+
+  private static void exampleIntsToList() {
+    log.debug("Example #2");
+    int[] array = new Random().ints(5, 0, 50).toArray();
+    log.debug(
+        "ints(long streamSize, int randomNumberOrigin, int randomNumberBound) => "
+            + Arrays.toString(array)
+            + "\n");
+  }
+}
--- src/main/java/com/malex/lecture_5_random/LineGeneration.java
+++ src/main/java/com/malex/lecture_5_random/LineGeneration.java
@@ -1,50 +1,44 @@
 package com.malex.lecture_5_random;
 
 import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
+import lombok.extern.log4j.Log4j;
 
+@Log4j
 public class LineGeneration {
-    private static final int MAXIMUM_NUMBER_APARTMENTS = 4;
 
-    public static void main(String[] args) {
-//        LineGeneration generation = new LineGeneration();
-//        System.out.println(generation.stringGeneration(10));
+  public static void main(String[] args) {
 
+    log.debug(stringGeneration(10) + "\n");
+    log.debug(stringGeneration(7) + "\n");
+    log.debug(stringGeneration(2) + "\n");
 
-//        BigInteger number = new BigInteger(String.valueOf(Integer.MAX_VALUE));
-//        System.out.println(number);
-//        BigInteger multiply = number.multiply(new BigInteger(String.valueOf(4)));
-//        System.out.println(multiply);
-        // 8_589_934_588
-        // 8_589_934_588
-        // 23_000_000_000
+    try {
+      log.debug(stringGeneration(ThreadLocalRandom.current().nextInt(-10, 0)) + "\n");
+    } catch (Exception ex) {
+      log.error(ex.getMessage());
+    }
+  }
 
-        long maxLimit = Integer.MAX_VALUE;
-        long apartment = maxLimit * MAXIMUM_NUMBER_APARTMENTS;
+  private static String stringGeneration(int numberOfCharacters) {
 
-        System.out.println(apartment);
-
-        long num1 = Integer.MAX_VALUE;
-        long num2 = Integer.MAX_VALUE;
-        System.out.println("Maximum floor limit (Integer.MAX_VALUE ) : " + num2);
-        System.out.println("The maximum limit of apartments in the house (Integer.MAX_VALUE * 4) : " + num2 * 4);
-        System.out.println(Long.MAX_VALUE);
-
+    if (numberOfCharacters <= 0) {
+      throw new ArithmeticException("Incorrect numberOfCharacters: " + numberOfCharacters);
     }
 
-    public String stringGeneration(int numberOfCharacters) {
-        if (numberOfCharacters <= 0) {
-            throw new ArithmeticException("Incorrect numberOfCharacters: " + numberOfCharacters);
-        }
-        StringBuilder sb = new StringBuilder();
-        char[] chars = {'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', 'a', 's',
-                'd', 'f', 'g', 'h', 'j', 'k', 'l', 'z', 'x', 'c', 'v', 'b', 'n', 'm',
-                'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', 'A', 'S', 'D', 'F',
-                'G', 'H', 'J', 'K', 'L', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', '_',
-                '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
-        int limit = chars.length - 1;
-        for (int i = 0; i < numberOfCharacters; i++) {
-            sb.append(chars[new Random().nextInt(limit)]);
-        }
-        return sb.toString();
+    StringBuilder sb = new StringBuilder();
+    char[] chars = {
+      'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l',
+      'z', 'x', 'c', 'v', 'b', 'n', 'm', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', 'A', 'S',
+      'D', 'F', 'G', 'H', 'J', 'K', 'L', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', '_', '0', '1', '2', '3',
+      '4', '5', '6', '7', '8', '9'
+    };
+
+    int limit = chars.length - 1;
+    for (int i = 0; i < numberOfCharacters; i++) {
+      sb.append(chars[new Random().nextInt(limit)]);
     }
+
+    return sb.toString();
+  }
 }
