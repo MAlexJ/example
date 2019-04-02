6431e0840ad94a83b6326cc8dd6a8b05ade3978a
malexj <alexmaximovj@gmail.com>
2019-04-02 22:37:27 +0300

    Add the example StringTokenizen

---
+++ src/main/java/com/malex/lecture_5_String/example_16_StringTokenizer/StringTokenizerExample.java
@@ -0,0 +1,24 @@
+package com.malex.lecture_5_String.example_16_StringTokenizer;
+
+import java.util.Arrays;
+import java.util.StringTokenizer;
+import lombok.extern.log4j.Log4j;
+
+@Log4j
+public class StringTokenizerExample {
+
+  private static final String STRING = "1:23:4:546:21:0:3";
+
+  public static void main(String[] args) {
+
+    StringTokenizer stk = new StringTokenizer(STRING, ":");
+
+    String[] ar = new String[stk.countTokens()];
+
+    for (int i = 0; i < ar.length; i++) {
+      ar[i] = stk.nextToken();
+    }
+
+    log.debug(Arrays.toString(ar));
+  }
+}
