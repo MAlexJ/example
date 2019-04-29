8578e39a5e56e2dd3cd102866e05ed044db3f027
malexj <alexmaximovj@gmail.com>
2019-04-29 23:41:01 +0300

    Add example of CompletableFuture

---
+++ src/main/java/com/malex/lecture_25_Java_8_Futures/example_01_CompletableFuture/SimpleExample.java
@@ -0,0 +1,42 @@
+package com.malex.lecture_25_Java_8_Futures.example_01_CompletableFuture;
+
+import lombok.extern.log4j.Log4j;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+@Log4j
+public class SimpleExample {
+
+    public static void main(String[] args) throws Exception {
+
+        /*
+         * Returns a new CompletableFuture that is asynchronously completed
+         * by a task running in the {@link ForkJoinPool#commonPool()} with
+         * the value obtained by calling the given Supplier.
+         */
+        CompletableFuture<String> future = CompletableFuture.supplyAsync(() -> "Hi");
+        log.debug(future.get());
+
+        future = CompletableFuture
+                .supplyAsync(() -> "Bye!", Executors.newCachedThreadPool());
+        log.debug(future.get());
+
+        /*
+         *Returns a new CompletableFuture that is asynchronously completed
+         * by a task running in the {@link ForkJoinPool#commonPool()} after
+         * it runs the given action.
+         */
+        CompletableFuture<Void> futureAsync = CompletableFuture.runAsync(() -> log.debug("Hi!"));
+        log.debug(futureAsync.get(1, TimeUnit.SECONDS));
+
+        futureAsync = CompletableFuture
+                .runAsync(() -> log.debug("Bye!"), Executors.newCachedThreadPool());
+        log.debug(futureAsync.get(1, TimeUnit.SECONDS));
+
+        if (futureAsync.isDone()) {
+            System.exit(0);
+        }
+    }
+}
