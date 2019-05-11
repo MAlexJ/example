7c13bc2b553b3212b227a4d41eac1eca0dbe5f3b
malexj <alexmaximovj@gmail.com>
2019-05-11 22:47:06 +0300

    Fix

---
+++ src/main/java/com/malex/lecture_25_Java_8/example_18_JoinStringArrayExample/JoinStringArrayExample.java
@@ -0,0 +1,48 @@
+package com.malex.lecture_25_Java_8.example_18_JoinStringArrayExample;
+
+import lombok.extern.log4j.Log4j;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * Title: Java 8 – Join String Array Example
+ * Link: https://howtodoinjava.com/java-8/java-8-join-string-array-example/
+ *
+ * @author malex
+ * @since Oct 14, 2017
+ */
+@Log4j
+public class JoinStringArrayExample {
+
+    /**
+     * Java examples to join string array to produce single String.
+     */
+    public static void main(String[] args) {
+
+        // java 8 -> String.join(..)
+        stringJoin();
+
+        // Stream -> Collectors.joining(.....)
+        collectorsJoining();
+    }
+
+    private static void stringJoin() {
+        String[] strArray = {"How", "To", "Do", "In", "Java"};
+
+        String joinedString = String.join(", ", strArray);
+
+        log.info(joinedString);
+    }
+
+    private static void collectorsJoining() {
+        List<String> numbers = Arrays.asList("How", "To", "Do", "In", "Java");
+
+        String joinedString = numbers
+                .stream()
+                .collect(Collectors.joining(", ", "[", "]"));
+
+        log.info(joinedString);
+    }
+}
