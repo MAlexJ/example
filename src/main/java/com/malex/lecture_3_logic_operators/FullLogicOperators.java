7bfb96b0ad3bcead1d8725be012dcec77bb11a5c
malexj <alexmaximovj@gmail.com>
2021-06-21 01:20:14 +0300

    Add tests

--- src/main/java/com/malex/lecture_3_logic_operators/FullLogicOperators.java
+++ src/main/java/com/malex/lecture_3_logic_operators/FullLogicOperators.java
@@ -1,127 +1,79 @@
 package com.malex.lecture_3_logic_operators;
 
-import org.junit.After;
-import org.junit.Before;
+import lombok.extern.java.Log;
 import org.junit.Test;
 
-import static junit.framework.TestCase.assertEquals;
-import static junit.framework.TestCase.assertFalse;
-import static junit.framework.TestCase.assertTrue;
+import java.util.function.UnaryOperator;
+
+import static org.mockito.Mockito.*;
 
 /**
  * This class use of full logic operators like: || and &&
  */
+@Log
 public class FullLogicOperators {
 
-	/**
-	 * Constant
-	 */
-	private static final int LIMIT = 10;
+    // -------------------------
+    //   A   |    B   |  A || B
+    // -------------------------
+    // True  | False  |  True
+    // False | True   |  True
+    // False | False  |  False
+    // True  | True   |  True
+    boolean simpleOr(boolean one, boolean two) {
+        return checkFirstNumber(one) || checkSecondNumber(two);
+    }
 
-	/**
-	 * flag
-	 */
-	private boolean firstMethod;
-	private boolean secondMethod;
+    // -------------------------
+    //   A   |    B   |  A && B
+    // -------------------------
+    // True  | False  |  False
+    // False | True   |  False
+    // False | False  |  False
+    // True  | True   |  True
+    boolean simpleAnd(boolean one, boolean two) {
+        return checkFirstNumber(one) && checkSecondNumber(two);
+    }
 
-	private boolean checkFirstNumber(int num) {
-		// The method is invoke
-		this.firstMethod = true;
-		return num < LIMIT;
-	}
+    boolean checkFirstNumber(boolean b) {
+        return b;
+    }
 
-	private boolean checkSecondNumber(int num) {
-		// The method is invoke
-		this.secondMethod = true;
-		return num > LIMIT;
-	}
+    boolean checkSecondNumber(boolean b) {
+        return b;
+    }
 
-	@Before
-	public void before() {
-		this.firstMethod = false;
-		this.secondMethod = false;
-	}
+    @Test
+    public void runTest() {
+        testSimpleOr(false, false, mock -> {
+            verify(mock).checkFirstNumber(false);
+            verify(mock).checkSecondNumber(false);
+            return mock;
+        });
 
-	@After
-	public void after() {
-		this.firstMethod = false;
-		this.secondMethod = false;
-	}
+        testSimpleOr(true, true, mock -> {
+            verify(mock).checkFirstNumber(true);
+            verify(mock, never()).checkSecondNumber(true);
+            return mock;
+        });
 
-	// -------------------------
-	//   A   |    B   |  A || B
-	// -------------------------
-	// True  | False  |  True
-	// False | True   |  True
-	// False | False  |  False
-	// True  | True   |  True
+        testSimpleOr(true, false, mock -> {
+            verify(mock).checkFirstNumber(true);
+            verify(mock, never()).checkSecondNumber(false);
+            return mock;
+        });
 
-	@Test
-	public void test_1() {
-		assertEquals(checkFirstNumber(1) || checkSecondNumber(11), true);
-		assertTrue(this.firstMethod);
-		assertFalse(this.secondMethod);
-	}
+        testSimpleOr(false, true, mock -> {
+            verify(mock).checkFirstNumber(false);
+            verify(mock).checkSecondNumber(true);
+            return mock;
+        });
+    }
 
-	@Test
-	public void test_2() {
-		assertEquals(checkFirstNumber(1) || checkSecondNumber(1), true);
-		assertTrue(this.firstMethod);
-		assertFalse(this.secondMethod);
-	}
-
-	@Test
-	public void test_3() {
-		assertEquals(checkFirstNumber(100) || checkSecondNumber(11), true);
-		assertTrue(this.firstMethod);
-		assertTrue(this.secondMethod);
-	}
-
-	@Test
-	public void test_4() {
-		assertEquals(checkFirstNumber(100) || checkSecondNumber(1), false);
-		assertTrue(this.firstMethod);
-		assertTrue(this.secondMethod);
-	}
-
-	// -------------------------
-	//   A   |    B   |  A && B
-	// -------------------------
-	// True  | False  |  False
-	// False | True   |  False
-	// False | False  |  False
-	// True  | True   |  True
-
-	@Test
-	public void test_5() {
-		// true && false
-		assertEquals(checkFirstNumber(1) && checkSecondNumber(1), false);
-		assertTrue(this.firstMethod);
-		assertTrue(this.secondMethod);
-	}
-
-	@Test
-	public void test_6() {
-		// false && true
-		assertEquals(checkFirstNumber(100) && checkSecondNumber(11), false);
-		assertTrue(this.firstMethod);
-		assertFalse(this.secondMethod); // #2: false
-	}
-
-	@Test
-	public void test_7() {
-		// false && true
-		assertEquals(checkFirstNumber(100) && checkSecondNumber(1), false);
-		assertTrue(this.firstMethod);
-		assertFalse(this.secondMethod); // #2: false
-	}
-
-	@Test
-	public void test_8() {
-		// true && true
-		assertEquals(checkFirstNumber(1) && checkSecondNumber(11), true);
-		assertTrue(this.firstMethod);
-		assertTrue(this.secondMethod);
-	}
+    private void testSimpleOr(boolean first, boolean second, UnaryOperator<FullLogicOperators> function) {
+        FullLogicOperators mock = spy(FullLogicOperators.class);
+        mock.simpleOr(first, second);
+        function.apply(mock);
+    }
 
 }
