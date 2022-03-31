984bed8667ec1de2c9aa215fd2937b4094f9e0c0
malexj <alexmaximovj@gmail.com>
2022-03-31 11:50:56 +0300

    Update pom dependensies and added evicting queue sample

---
+++ src/main/java/com/malex/lecture_26_SDL/evicting_queue/EvictingQueueSample.java
@@ -0,0 +1,43 @@
+package com.malex.lecture_26_SDL.evicting_queue;
+
+import com.google.common.collect.EvictingQueue;
+import lombok.extern.log4j.Log4j;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+
+import static org.junit.Assert.assertArrayEquals;
+
+@Log4j
+public class EvictingQueueSample {
+
+    @Test
+    public void test() {
+        EvictingQueue<String> queue = EvictingQueue.create(3);
+        queue.add("1");
+        queue.add("2");
+        queue.add("3");
+        log.info(queue);
+
+        assertArrayEquals(new LinkedList<>(Arrays.asList("1", "2", "3")).toArray(), queue.toArray());
+
+        /**
+         * Adds the given element to this queue. If the queue is currently full,
+         * the element at the head of the queue is evicted to make room.
+         * Returns: true always
+         */
+        queue.add("4");
+        log.info(queue);
+        assertArrayEquals(new LinkedList<>(Arrays.asList("2", "3", "4")).toArray(), queue.toArray());
+
+        /**
+         * Adds the given element to this queue. If the queue is currently full,
+         * the element at the head of the queue is evicted to make room.
+         * Returns: true always
+         */
+        queue.offer("5");
+        log.info(queue);
+        assertArrayEquals(new LinkedList<>(Arrays.asList("3", "4", "5")).toArray(), queue.toArray());
+    }
+}
