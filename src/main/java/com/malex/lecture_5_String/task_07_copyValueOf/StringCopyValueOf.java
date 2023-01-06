463c472d3d1a3f1dbe919b81b37ec5cc596b3170
malexj <alexmaximovj@gmail.com>
2023-01-06 14:00:50 +0200

    Add UnaryOperator sample

--- src/main/java/com/malex/lecture_5_String/task_07_copyValueOf/StringCopyValueOf.java
+++ src/main/java/com/malex/lecture_5_String/task_07_copyValueOf/StringCopyValueOf.java
@@ -1,21 +1,25 @@
 package com.malex.lecture_5_String.task_07_copyValueOf;
 
+import lombok.extern.java.Log;
+import org.junit.Test;
+
 /**
  * Created by Alex on 06.03.2015.
  */
-public class App_01_copyValueOf {
+@Log
+public class StringCopyValueOf {
 
-    public static void main(String args[]) {
+    @Test
+    public void test() {
         char[] str1 = {'h', 'e', 'l', 'l', 'o', ' ', 'w', 'o', 'r', 'l', 'd'};
         String str2 = "";
 
         //This method returns a String that contains the characters of the character array.
         str2 = str2.copyValueOf(str1);
-        System.out.println("Returned String: " + str2);
+        log.info("Returned String: " + str2);
 
         //This method returns a String that contains the characters of the character array.
         str2 = str2.copyValueOf(str1, 2, 6);
-        System.out.println("Returned String: " + str2);
-
+        log.info("Returned String: " + str2);
     }
 }
