14c87390f2184a71fddc817fa4b2bb94a59264e6
malexj <alexmaximovj@gmail.com>
2024-02-05 22:36:46 +0200

    Add sample cuncurency empty samples

---
+++ src/main/java/com/malex/lecture_12_lvti_var/LocalVariableTypeInference.java
@@ -0,0 +1,18 @@
+package com.malex.lecture_12_lvti_var;
+
+import com.malex.utils.AbstractUtils;
+import org.junit.Test;
+
+/**
+ * The Java Local Variable Type Inference (LVTI) <br>
+ * Java 10
+ *
+ * <p>Links: https://openjdk.org/jeps/286 <br>
+ * https://habr.com/ru/articles/438206/ <br>
+ * https://habr.com/ru/companies/otus/articles/688722/ <br>
+ */
+public class LocalVariableTypeInference extends AbstractUtils {
+
+  @Test
+  public void run() {}
+}
---
+++ src/main/java/com/malex/lecture_15_concurrency/example_12_cyclic_barrier/CyclicBarrier.java
@@ -0,0 +1,13 @@
+package com.malex.lecture_15_concurrency.example_12_cyclic_barrier;
+
+import com.malex.utils.AbstractUtils;
+import org.junit.Test;
+
+public class CyclicBarrier extends AbstractUtils {
+
+    @Test
+    public void run(){
+
+    }
+
+}
