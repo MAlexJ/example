b1b925a468343b4ab3c28c5833c79c741be11b1b
malexj <alexmaximovj@gmail.com>
2024-02-13 15:31:53 +0200

    Add recodrs sample

--- src/main/java/com/malex/lecture_11_record/RecordStaticVariablesAndMethods.java
+++ src/main/java/com/malex/lecture_11_record/RecordStaticVariablesAndMethods.java
@@ -1,6 +1,10 @@
 package com.malex.lecture_11_record;
 
+import static junit.framework.TestCase.assertEquals;
+
 import com.malex.utils.AbstractUtils;
+import java.util.stream.IntStream;
+import org.junit.Test;
 
 /**
  * Tutorial: Tutorial: Java 14 Record Keyword
@@ -8,4 +12,30 @@ import com.malex.utils.AbstractUtils;
  * <p>link: <a href="https://www.baeldung.com/java-record-keyword#static">Static Variables &
  * Methods</a>
  */
-public class RecordStaticVariablesAndMethods extends AbstractUtils {}
+public class RecordStaticVariablesAndMethods extends AbstractUtils {
+
+  @Test
+  public void sample() {
+    String template = MRecord.TEMPLATE;
+    println("MRecord.TEMPLATE :", template);
+
+    String method = MRecord.staticMethod();
+    println("static method :", method);
+
+    var mRecord = new MRecord();
+    IntStream.range(0, 5).forEach(num -> assertEquals(num, mRecord.noneStaticMethod(num)));
+  }
+
+  private record MRecord() {
+
+    private static final String TEMPLATE = "String template";
+
+    private static String staticMethod() {
+      return TEMPLATE;
+    }
+
+    public int noneStaticMethod(int num) {
+      return num;
+    }
+  }
+}
