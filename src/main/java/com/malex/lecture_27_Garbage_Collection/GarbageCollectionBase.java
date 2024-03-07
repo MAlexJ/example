5820f1a258c0f6f79861ca891d738044aa1db26b
malexj <alexmaximovj@gmail.com>
2024-03-07 12:10:02 +0200

    New

---
+++ src/main/java/com/malex/lecture_27_Garbage_Collection/GarbageCollectionBase.java
@@ -0,0 +1,6 @@
+package com.malex.lecture_27_Garbage_Collection;
+
+/**
+ * Link: https://habr.com/ru/companies/otus/articles/776342/
+ */
+public class GarbageCollectionBase {}
---
+++ src/main/java/com/malex/lecture_27_Garbage_Collection/LinkReferencesBase.java
@@ -0,0 +1,4 @@
+package com.malex.lecture_27_Garbage_Collection;
+
+/** link: https://www.youtube.com/watch?v=5HC8qVHQPi8 */
+public class LinkReferencesBase {}
