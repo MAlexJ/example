5e400b06fa8b7a5cc2954b5aedc77d18148b85fa
malexj <alexmaximovj@gmail.com>
2024-06-10 19:33:13 +0300

    Add weak references

---
+++ src/main/java/com/malex/lecture_1_object_peferences/BaseReferences.java
@@ -0,0 +1,32 @@
+package com.malex.lecture_1_object_peferences;
+
+import com.malex.utils.AbstractUtils;
+import java.lang.ref.PhantomReference;
+import java.lang.ref.ReferenceQueue;
+import java.lang.ref.SoftReference;
+import java.lang.ref.WeakReference;
+import org.junit.Test;
+
+public class BaseReferences extends AbstractUtils {
+
+  @Test
+  public void explanation() {
+    // 1. Strong reference
+    Object object = new Object();
+
+    // 2. Soft reference
+    SoftReference<Object> softReference = new SoftReference<>(object);
+
+    // 3. Weak reference
+    WeakReference<Object> weakReference = new WeakReference<>(object);
+
+    // 4. Phantom reference
+    ReferenceQueue<Object> referenceQueue = new ReferenceQueue<>();
+    PhantomReference<Object> phantomReference = new PhantomReference<>(object, referenceQueue);
+
+    println("Strong reference:", object);
+    println("Soft reference:", softReference.get());
+    println("Weak reference:", weakReference.get());
+    println("Phantom reference:", phantomReference.get());
+  }
+}
