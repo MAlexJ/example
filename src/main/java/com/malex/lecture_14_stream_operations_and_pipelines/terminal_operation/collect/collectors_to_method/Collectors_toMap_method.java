adf5b0faacef9a660960812ac2a66bb6902a8e21
malexj <alexmaximovj@gmail.com>
2024-09-15 11:06:55 +0300

    fix

--- src/main/java/com/malex/lecture_14_stream_operations_and_pipelines/terminal_operation/collect/collectors_to_method/Collectors_toMap_method.java
+++ src/main/java/com/malex/lecture_14_stream_operations_and_pipelines/terminal_operation/collect/collectors_to_method/Collectors_toMap_method.java
@@ -1,3 +1,19 @@
 package com.malex.lecture_14_stream_operations_and_pipelines.terminal_operation.collect.collectors_to_method;
 
-public class Collectors_toMap_method {}
+import com.malex.utils.AbstractUtils;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import org.junit.Test;
+
+public class Collectors_toMap_method extends AbstractUtils {
+
+  @Test
+  public void toMap() {
+    Map<Integer, Integer> map =
+        Stream.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10).collect(Collectors.toMap(k -> k, v -> v));
+
+    println("class:", map.getClass().getName());
+    println("map:", map);
+  }
+}
