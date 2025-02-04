3d936b11d37827a845980fe2cfa55ceaa903e968
malex <alexmaximovj@gmail.com>
2025-02-04 16:18:25 +0200

    add clean sample

--- src/main/java/com/malex/lecture_13_collection_api/map/methods_map/Clear_method.java
+++ src/main/java/com/malex/lecture_13_collection_api/map/methods_map/Clear_method.java
@@ -1,3 +1,43 @@
 package com.malex.lecture_13_collection_api.map.methods_map;
 
-public class Clear_method {}
+import static junit.framework.TestCase.assertTrue;
+
+import com.malex.utils.AbstractUtils;
+import java.util.HashMap;
+import java.util.Map;
+import org.junit.Test;
+
+public class Clear_method extends AbstractUtils {
+
+  /*
+   * Removes all of the mappings from this map (optional operation).
+   * The map will be empty after this call returns.
+   */
+  @Test
+  public void clear() {
+    Map<Integer, String> map =
+        new HashMap<>() {
+          {
+            put(1, "A");
+          }
+
+          {
+            put(2, "B");
+          }
+        };
+
+    println("map", map);
+
+    map.clear();
+
+    assertTrue(map.isEmpty());
+  }
+
+  @Test(expected = UnsupportedOperationException.class)
+  public void clean_unmodifiedMap() {
+    Map<Integer, String> map = Map.of(1, "A");
+    println("map", map);
+
+    map.clear();
+  }
+}
